07dc0fd3f7604972f76024d0d99dd46e604ec1bd
Kaushal Prajapat <116460669+KP-2903@users.noreply.github.com>
2024-09-13 13:33:34 +0530

    Add files via upload

--- Sum of Series with intent/MainActivity.java
+++ Sum of Series with intent/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -12,29 +13,46 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
-    TextView tv1;
-    Button bt1;
+    Button bt;
+    EditText n;
+    TextView t1,s1,s2,sol1,sol;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
 
-        tv1=findViewById(R.id.tv1);
-        bt1=findViewById(R.id.bt1);
+        bt=findViewById(R.id.bt);
+        n=findViewById(R.id.n);
+        t1=findViewById(R.id.t1);
+        s1=findViewById(R.id.s1);
+        s2=findViewById(R.id.s2);
+        sol1=findViewById(R.id.sol1);
+        sol=findViewById(R.id.sol);
 
-        Intent intent2= getIntent();
-        String s2= intent2.getStringExtra("solution");
 
-        bt1.setOnClickListener(new View.OnClickListener() {
+        bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent_back=new Intent(getApplicationContext(),MainActivity.class);
-                startActivity(intent_back);
+                float num=Integer.parseInt(n.getText().toString());
+                float sum=0;
+                for(float i=1;i<=num;i++){
+                    sum+=1/i;
+                }
+                sol1.setText(Float.toString(sum));
+                String s1=Float.toString(sum);
+                Intent intent1=new Intent(getApplicationContext(), MainActivity2.class);
+                intent1.putExtra("solution",s1);
+                startActivity(intent1);
+
             }
+            Intent intentt=getIntent();
         });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
