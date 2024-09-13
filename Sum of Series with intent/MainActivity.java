36888630ccc02aa77044211e7e5e8dc4e5ff826f
Kaushal Prajapat <116460669+KP-2903@users.noreply.github.com>
2024-09-13 13:31:52 +0530

    Create MainActivity.java

---
+++ Sum of Series with intent/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.series;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    TextView tv1;
+    Button bt1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+
+        tv1=findViewById(R.id.tv1);
+        bt1=findViewById(R.id.bt1);
+
+        Intent intent2= getIntent();
+        String s2= intent2.getStringExtra("solution");
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent_back=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent_back);
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
